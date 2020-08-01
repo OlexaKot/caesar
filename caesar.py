e0243978ef58bfd9b72e0859e728e13d93314a4b
Oleksandr Kotusenko <32229936+OlexaKot@users.noreply.github.com>
2020-08-01 14:46:46 -0400

    Update caesar.py
    Added the crack() funtion which is used to crack the encrypted message using the "brute-force" approach.

--- caesar.py
+++ caesar.py
@@ -7,8 +7,8 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 def encrypt(plaintext, key):
     ciphertext = ''
     for letter in plaintext:
-        is_upper = False
         if letter.lower() in alphabet:
+            is_upper = False
             if letter not in alphabet:
                 is_upper = True
             letter = letter.lower()
@@ -24,15 +24,33 @@ def encrypt(plaintext, key):
 def decrypt(ciphertext, key):
     plaintext = ''
     for letter in ciphertext:
-        is_upper = False
         if letter.lower() in alphabet:
+            is_upper = False
             if letter not in alphabet:
                 is_upper = True
             letter = letter.lower()
-            encrypted_letter = alphabet[(alphabet.index(letter) - key) % 26]
+            decrypted_letter = alphabet[(alphabet.index(letter) - key) % 26]
             if is_upper == True:
-                encrypted_letter = encrypted_letter.upper()
-            plaintext += encrypted_letter
+                decrypted_letter = decrypted_letter.upper()
+            plaintext += decrypted_letter
         else:
             plaintext += letter
     return plaintext
+
+# Crack
+def crack(ciphertext):
+    for version in range(0, 26):
+        cracked = ''
+        for letter in ciphertext:
+            if letter.lower() in alphabet:
+                is_upper = False
+                if letter not in alphabet:
+                    is_upper = True
+                letter = letter.lower()
+                decrypted_letter = alphabet[(alphabet.index(letter) + version) % 26]
+                if is_upper == True:
+                    decrypted_letter = decrypted_letter.upper()
+                cracked += decrypted_letter
+            else:
+                cracked += letter
+        print("+" + str(version) + " " + cracked)
