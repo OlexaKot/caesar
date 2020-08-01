7bb99ff1016d8d465b0b33051fecd16a8a2d7770
Oleksandr Kotusenko <32229936+OlexaKot@users.noreply.github.com>
2020-07-31 22:39:14 -0400

    Add files via upload

---
+++ caesar.py
@@ -0,0 +1,37 @@
+# Caesar Cipher
+
+# Setup
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+# Encryption
+def encrypt(plaintext, shift):
+    ciphertext = ''
+    for letter in plaintext:
+        is_upper = False
+        if letter.lower() in alphabet:
+            if letter not in alphabet:
+                is_upper = True
+            letter = letter.lower()
+            encrypted_letter = alphabet[(alphabet.index(letter) + shift) % 26]
+            if is_upper == True:
+                encrypted_letter = encrypted_letter.upper()
+            ciphertext += encrypted_letter
+        else:
+            ciphertext += letter
+    return ciphertext
+    
+def decrypt(ciphertext, shift):
+    plaintext = ''
+    for letter in ciphertext:
+        is_upper = False
+        if letter.lower() in alphabet:
+            if letter not in alphabet:
+                is_upper = True
+            letter = letter.lower()
+            encrypted_letter = alphabet[(alphabet.index(letter) - shift) % 26]
+            if is_upper == True:
+                encrypted_letter = encrypted_letter.upper()
+            plaintext += encrypted_letter
+        else:
+            plaintext += letter
+    return plaintext
